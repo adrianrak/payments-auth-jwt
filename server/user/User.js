6d2e9868dbae0865c7ce94be0733591b505fb2ae
vladyani <vladyslavyantsov@gmail.com>
2018-08-21 21:49:52 +0100

    created date added to server

--- server/user/User.js
+++ server/user/User.js
@@ -1,12 +1,16 @@
-var mongoose = require('mongoose');  
-var UserSchema = new mongoose.Schema({  
-  name: String,
-  email: String,
-  password: String,
-  postalCode: String,
-  creditCardNumber: Number,
-  expiryDateOfCard: String,
-  cvv: String
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    postalCode: String,
+    creditCardNumber: Number,
+    expiryDateOfCard: String,
+    cvv: String,
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
 });
 mongoose.model('User', UserSchema);
 

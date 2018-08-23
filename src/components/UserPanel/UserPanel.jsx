3cf27a591b57792481c1254d43c1ab7aaab3b907
Adrian Rak <adrrak@tlen.pl>
2018-08-23 09:04:15 +0200

    add braintree in frontend

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,25 +1,39 @@
 import React, {Component} from 'react';
 import {ToastContainer, toast} from 'react-toastify';
 import ApiService from '../../service/api.service';
+import DropIn from "braintree-web-drop-in-react";
 
 export default class UserPanel extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    }
-
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    };
-
     render() {
-        return (
-            <div style={{background:'transparent'}}>
-                <p>Hello User</p>
-            </div>
-        );
-    }
+		return (
+			<div style={{ textAlign: "center" }}>
+				<DropIn
+					options={{
+						authorization:
+						'sandbox_25zg57sb_mhjjzn94dhchd3zh'
+					}}
+				/>
+				<button>Buy Now!</button>
+			</div>
+		);
+	}
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //     }
+    // }
+
+    // handleChange = (event) => {
+    //     this.setState({
+    //         [event.target.name]: event.target.value
+    //     })
+    // };
+
+    // render() {
+    //     return (
+    //         <div style={{background:'transparent'}}>
+    //             <p>Hello User</p>
+    //         </div>
+    //     );
+    // }
 }
